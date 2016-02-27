3b4fb0b413daae1f0a6472f7fcb3677356a27e41
L16T12 <L16T12>
2016-02-27 09:57:51 +0000

    Rotate Funtion add by Jinliang

--- inc/Image.h
+++ inc/Image.h
@@ -1,7 +1,7 @@
-/*********************************************************************/ 
-/* Image.hpp: header file for image class                    		     */ 
+/*********************************************************************/
+/* Image.hpp: header file for image class                    		     */
 /*                                                                   */
-/*********************************************************************/ 
+/*********************************************************************/
 #ifndef IMAGE_HPP
 #define IMAGE_HPP
 #define SLEN    80		/* maximum length of file names */
@@ -10,23 +10,30 @@
 #include <vector>
 using std::vector;
 
-	class IMAGE{
-	public:
-	int WIDTH;
-	int HEIGHT;
-	vector<vector<unsigned char> > R;
-	vector<vector<unsigned char> > G;
-	vector<vector<unsigned char> > B;
- 
-	public:
-
-	IMAGE();
-	IMAGE(int w, int r);//construction declaration
-	~IMAGE();//destructor declaration
-	
-	int SaveImage(const char fname[SLEN]);
-  void BlackNWhite();	
-
-	};
+class IMAGE{
+public:
+    int WIDTH;
+    int HEIGHT;
+    vector<vector<unsigned char> > R;
+    vector<vector<unsigned char> > G;
+    vector<vector<unsigned char> > B;
+    
+public:
+    
+    IMAGE();
+    IMAGE(int w, int r);//construction declaration
+    ~IMAGE();//destructor declaration
+    
+    int SaveImage(const char fname[SLEN]);
+    void BlackNWhite();
+    
+    //image rotation by radian and rotation center
+    void rotate(double radian, int offsetX, int offsetY);
+    //crop image by 2 set of coordinate
+    void cropIMG(int starX,int starY, int endX, int endY);
+    //find the edge coordinate
+    void findEdge(int edge[4]);
+    
+};
 
 #endif
