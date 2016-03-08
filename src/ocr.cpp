20a956cd0b438b44d4e91a2f4079ca82696c1cdb
L16T12 <L16T12>
2016-03-08 23:42:41 +0000

    anges from old ocr.cpp ----------------------------------------------------------------------

--- src/ocr.cpp
+++ src/ocr.cpp
@@ -5,65 +5,56 @@
 #include <string>
 #include "image.h"
 #include "ocr.h"
+#include <fstream>
+#include <iostream>
 
 //Constructor
 OCR::OCR(){
-reference = #include "averageIntensities.txt";
+//reference = #include "averageIntensities.txt";
 }
 
 //Desrtructor
-OCR:~OCR(){
+OCR::~OCR(){
 
 }
 
 //Functions
 
-void OCR::charCrop(IMAGE im, vector<vector<char> > &charVec){
+void OCR::charCrop(Image im, std::vector<std::vector<char> > &charVec){
 
-  IMAGE newImage;
-  vector<char> text;
-  vector<unsigned char> TempR,TempG,TempB;
-  for(int i=0; i<im.HEIGHT; i+=56){
-    for(int j=0;j<im.WIDTH; j+=30){
+  Image newImage(30,56);
+  std::vector<char> text;
+  
+  for(int i=0; i<im.getHeight(); i+=56){
+    for(int j=0;j<im.getWidth(); j+=30){
       for(int m=0;m<56;m++){
         for(int n=0;n<30;n++){
-          TempR.push_back(im.R[m+i][n+j]);
-          TempG.push_back(im.G[m+i][n+j]);
-          TempB.push_back(im.B[m+i][n+j]);
+          newImage.setPixel(j,i,Image::R, im.getPixel(j,i,Image::R));
+          newImage.setPixel(j,i,Image::G, im.getPixel(j,i,Image::G));
+          newImage.setPixel(j,i,Image::B, im.getPixel(j,i,Image::B));
         }
-        newImage.R.push_back(TempR);
-        newImage.G.push_back(TempG);
-        newImage.B.push_back(TempB);
-        TempR.clear();
-        TempG.clear();
-        TempB.clear();
       }
-          newImage.WIDTH=30;
-          newImage.HEIGHT=56;
           text.push_back(charComp(newImage));
-          newImage.R.clear();
-          newImage.G.clear();
-          newImage.B.clear();
     }
     charVec.push_back(text);
     text.clear();
   }
 }
 
-char OCR::charComp(IMAGE im){
+char OCR::charComp(Image im){
 
   char output;
   float intensity;
   float refInt, difference, best=255.0;
-  string line,value;
+  std::string line, value;
   intensity = avgInt(im); 
-  ifstream reference ("../inc/averageIntensities.txt");
+  std::ifstream reference ("../inc/averageIntensities.txt");
   if(reference.is_open()){
    while(getline (reference,line)){
    value = line.substr(0,7);
    refInt = atof(value.c_str());
    difference = (refInt-intensity> 0) ? (refInt-intensity) : (intensity - refInt);
-   cout << difference;
+   std::cout << difference;
    if(difference < best) {
      best = difference;
      output = line[7];
@@ -71,19 +62,17 @@ char OCR::charComp(IMAGE im){
      }
      reference.close();
    }
-   cout << "The best value is : \n" << best << endl;
-   cout << "The intensity value is : \n" << intensity << endl;
    return output;
  }
  
-float OCR::avgInt(IMAGE im){
+float OCR::avgInt(Image im){
 
   float total = 0.0;
   int i,j;
   
-  for(i=0; i<im.HEIGHT; i++){
-    for(j=0;j<im.WIDTH;j++){
-      total += im.R[i][j] + im.G[i][j] + im.B[i][j];
+  for(i=0; i<im.getHeight(); i++){
+    for(j=0;j<im.getWidth();j++){
+      total +=  im.getPixel(j,i,Image::R)+ im.getPixel(j,i,Image::G) + im.getPixel(j,i,Image::B);
     }
   }
   
@@ -91,5 +80,5 @@ float OCR::avgInt(IMAGE im){
 }
 
 const char *getReference() {
-  return reference;
+  //return reference;
 }
